de4bd2aa5dee1c0edf4862cd04871426dd362a4b
rlhatcher <ronaldhatcher@mac.com>
2023-08-28 23:58:59 +0100

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import Link from 'next/link'
-import { draftMode } from 'next/headers'
-
-import Date from './date'
-import CoverImage from './cover-image'
-import Avatar from './avatar'
-import MoreStories from './more-stories'
-
-import { getAllPosts } from '@/lib/api'
-import HeroSection from './hero-section'
-import PostsSection from './posts-section'
+import { draftMode } from "next/headers";
+import { getAllPosts } from "@/lib/api";
+import HeroSection from "./hero-section";
+import PostsSection from "./posts-section";
 
 function Intro() {
   return (
@@ -20,63 +13,18 @@ function Intro() {
         A statically generated blog.
       </h2>
     </section>
-  )
-}
-
-function HeroPost({
-  title,
-  image,
-  date,
-  excerpt,
-  author,
-  slug,
-}: {
-  title: string
-  image: any
-  date: string
-  excerpt: string
-  author: any
-  slug: string
-}) {
-  return (
-    <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={title} slug={slug} image={image} />
-      </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link href={`/posts/${slug}`} className="hover:underline">
-              {title}
-            </Link>
-          </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <Date dateString={date} />
-          </div>
-        </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          {author && <Avatar name={author.name} picture={author.picture} />}
-        </div>
-      </div>
-    </section>
-  )
+  );
 }
 
 export default async function Page() {
-  const { isEnabled } = draftMode()
-  const allPosts = await getAllPosts(isEnabled)
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const { isEnabled } = draftMode();
+  const allPosts = await getAllPosts(isEnabled);
 
   return (
     <div className="container mx-auto px-5">
       <Intro />
-      {heroPost && (
-        <HeroSection />
-      )}
-      <PostsSection posts={allPosts}/>
-      <MoreStories morePosts={morePosts} />
+      <HeroSection />
+      <PostsSection posts={allPosts} />
     </div>
-  )
+  );
 }
