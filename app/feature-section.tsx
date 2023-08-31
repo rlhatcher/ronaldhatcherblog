d2a9ecbd560ae794ab4e21f5df1aa544c6e85979
rlhatcher <ronaldhatcher@mac.com>
2023-08-31 11:06:47 +0100

    styles

--- app/feature-section.tsx
+++ app/feature-section.tsx
@@ -26,25 +26,23 @@ const features = [
 
 export default function FeatureSection() {
   return (
-    <div className="bg-gray-100 py-24 sm:py-32">
+    <div className="bg-gray-100 rounded-xl py-24 sm:py-8">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Resources for rockets, robots and architecture
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
+            This site is the blah blah blah. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
             accusamus quisquam.
           </p>
         </div>
-        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
-          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto mt-4 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none bg-gray-100 lg:grid-cols-3">
             {features.map((feature) => (
               <div key={feature.name} className="flex flex-col">
-                <dt className="text-base font-semibold leading-7 text-gray-900">
-                  {/* <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
-                  </div> */}
+                <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                  <feature.icon className="h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
                   {feature.name}
                 </dt>
                 <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import FeatureSection from "./feature-section";
 
 function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col md:flex-row flex font-mono items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Ronald Hatcher.
       </h1>
-      <h2 className="text-center md:text-left text-xl font-mono mt-5 md:pl-8">
+      <h2 className="text-center md:text-left text-xl mt-5 md:pl-8">
         A statically generated blog.
       </h2>
     </section>
