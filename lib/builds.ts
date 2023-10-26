4439233d2be820b43f281f3d4b59ef2816e08f8a
rlhatcher <ronaldhatcher@mac.com>
2023-10-26 02:53:07 +0100

    lint

--- lib/builds.ts
+++ lib/builds.ts
@@ -64,7 +64,9 @@ export async function getBuildByName (
       title: frontmatter.title,
       date: frontmatter.date,
       image: frontmatter.image,
-      tags: frontmatter.tags
+      tags: frontmatter.tags,
+      description: '',
+      weight: 0
     },
     content
   }
