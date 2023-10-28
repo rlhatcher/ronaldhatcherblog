4df0b8f3a7c4b319ff3239f690c5b5496d6d4f98
rlhatcher <ronaldhatcher@mac.com>
2023-10-28 12:02:07 +0100

    cleanup

--- lib/api.ts
+++ lib/api.ts
@@ -55,14 +55,6 @@ async function fetchGraphQL (query: string, preview = false): Promise<any> {
   ).then(async (response) => await response.json())
 }
 
-function extractPost (fetchResponse: any): any {
-  return fetchResponse?.data?.postCollection?.items?.[0]
-}
-
-function extractPostEntries (fetchResponse: any): any[] {
-  return fetchResponse?.data?.postCollection?.items
-}
-
 function extractFeatureEntries (fetchResponse: any): any[] {
   return fetchResponse?.data?.featureCollection?.items
 }
@@ -83,38 +75,4 @@ export async function getAllFeatures (isDraftMode: boolean): Promise<any[]> {
     false
   )
   return extractFeatureEntries(entries)
-}
-
-export async function getPostAndMorePosts (
-  slug: string,
-  preview: boolean
-): Promise<any> {
-  const entry = await fetchGraphQL(
-    `query {
-      postCollection(where: { slug: "${slug}" }, preview: ${
-      preview ? 'true' : 'false'
-    }, limit: 1) {
-        items {
-          ${POST_GRAPHQL_FIELDS}
-        }
-      }
-    }`,
-    preview
-  )
-  const entries = await fetchGraphQL(
-    `query {
-      postCollection(where: { slug_not_in: "${slug}" }, order: date_DESC, preview: ${
-      preview ? 'true' : 'false'
-    }, limit: 2) {
-        items {
-          ${POST_GRAPHQL_FIELDS}
-        }
-      }
-    }`,
-    preview
-  )
-  return {
-    post: extractPost(entry),
-    morePosts: extractPostEntries(entries)
-  }
 }
