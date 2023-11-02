e4855fe5ba02c60d627bfd87d59c18ad08a401fd
rlhatcher <ronaldhatcher@mac.com>
2023-11-02 08:13:56 +0000

    interim

--- app/_components/FeatureItem.tsx
+++ app/_components/FeatureItem.tsx
@@ -1,8 +1,13 @@
+'use client'
 import React from 'react'
-import Avatar from './avatar'
+import { CldImage } from 'next-cloudinary'
 import Link from 'next/link'
 
-export default function FeatureItem ({ feature }: { feature: Feature }): React.JSX.Element {
+export default function FeatureItem ({
+  feature
+}: {
+  feature: Feature
+}): React.JSX.Element {
   return (
     <Link
       key={feature.name}
@@ -11,7 +16,13 @@ export default function FeatureItem ({ feature }: { feature: Feature }): React.J
     >
       <div className='flex flex-col cursor-pointer'>
         <dt className='flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900'>
-          <Avatar name={feature.name} picture={feature.icon} />
+          <CldImage
+            alt={feature.name}
+            className='h-6 w-6 flex-none rounded-full bg-white/10'
+            height={48}
+            width={48}
+            src={feature.icon}
+          />
         </dt>
         <dd className='relative isolate mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600'>
           <p className='flex-auto'>{feature.content}</p>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import { Inter, Red_Hat_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import { type Metadata } from 'next'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
 
 export const metadata: Metadata = {
   title: 'Ronald Hatcher',
