2c011d939b8eb27a6933aae3f6cda37ad3a0c2a4
rlhatcher <ronaldhatcher@mac.com>
2023-11-03 11:33:09 +0000

    upgrade to next 14 and fix feature section tags

--- app/_components/FeatureItem.tsx
+++ app/_components/FeatureItem.tsx
@@ -8,27 +8,30 @@ export default function FeatureItem ({
   feature: Feature
 }): React.JSX.Element {
   return (
-    <Link
-      key={feature.name}
-      href={`/${feature.name}`}
-      className='hover:underline'
-    >
+    <section>
       <div className='flex flex-col cursor-pointer'>
         <dt className='flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900'>
           <CoverImage
             title={feature.name}
             slug={feature.name}
             image={feature.icon}
-            className='h-12 w-12 flex-none bg-gray-50'
+            className='h-8 w-8 flex-none bg-gray-50'
           />
-          <h1 className='flex-auto text-xl font-semibold leading-7 text-gray-900'>
-            {feature.name}
-          </h1>
+          <h2 className='flex-auto text-xl font-semibold leading-7 text-gray-900'>
+              {feature.title}
+          </h2>
         </dt>
         <dd className='relative isolate mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600'>
-          <p className='flex-auto'>{feature.content}</p>
+        <Link
+              key={feature.name}
+              href={`/${feature.name}`}
+              className='hover:underline'
+            >
+              <span className='absolute inset-0' />
+          {feature.content}
+          </Link>
         </dd>
       </div>
-    </Link>
+    </section>
   )
 }
