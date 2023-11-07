29ba99bd3897ae06388370bf159d767cfb55b4dc
rlhatcher <ronaldhatcher@mac.com>
2023-11-07 19:52:44 +0000

    initial

--- app/_components/Gallery.tsx
+++ app/_components/Gallery.tsx
@@ -22,15 +22,11 @@ export default async function Gallery ({
   }
 
   return (
-    <div className='bg-white'>
-      <div className='mx-auto max-w-7xl overflow-hidden sm:px-6 lg:px-8'>
-        <h2 className='sr-only'>Gallery</h2>
-
-        <div className='-mx-px grid grid-cols-2 border-l border-gray-200 sm:mx-0 md:grid-cols-3 lg:grid-cols-4'>
-          {assets.map((asset) => (
-            <GalleryItem publicid={asset} key={asset} />
-          ))}
-        </div>
+    <div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
+      <div className='grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8'>
+        {assets.map((asset) => (
+          <GalleryItem publicid={asset} key={asset} />
+        ))}
       </div>
     </div>
   )
