e41c85804517d31552faaea583c4d05845373080
rlhatcher <ronaldhatcher@mac.com>
2023-11-12 22:33:25 +0000

    lighthouse fixes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.VERCEL_URL || 'http://localhost:3000',
+    siteUrl: process.env.DOMAIN_URL || 'http://localhost:3000',
     generateRobotsTxt: true,
     generateIndexSitemap: false,
 }
