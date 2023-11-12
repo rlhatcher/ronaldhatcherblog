1c89d01e42f968959892a30f8379ea02e8fa1821
rlhatcher <ronaldhatcher@mac.com>
2023-11-12 00:55:06 +0000

    front page tweaks

--- app/_components/FeatureSection.tsx
+++ app/_components/FeatureSection.tsx
@@ -7,10 +7,10 @@ export default function FeatureSection ({
   features: Feature[]
 }): React.JSX.Element {
   return (
-    <div className='overflow-hidden bg-gray-100 shadow sm:rounded-lg'>
-      <div className='px-4 py-5 sm:p-6'>
-        <div className='mx-auto max-w-2xl sm:mt-4 mt-4 lg:max-w-none'>
-          <dl className='grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3'>
+    <div className='overflow-hidden bg-gray-100 shadow sm:rounded-lg mb-2'>
+      <div className='px-4 sm:p-6'>
+        <div className='mx-auto max-w-2xl lg:max-w-none'>
+          <dl className='grid max-w-xl grid-cols-1 gap-x-8 gap-y-8 lg:max-w-none lg:grid-cols-3'>
             {features.map((feature) => (
               <FeatureItem feature={feature} key={feature.name} />
             ))}
--- app/_components/posts-section.tsx
+++ app/_components/posts-section.tsx
@@ -11,17 +11,17 @@ export default async function PostsSection ({
   if (posts == null) return []
 
   return (
-    <div className='rounded-2xl bg-gray-100 pt-12 sm:pt-6'>
+    <div className='rounded-2xl bg-gray-100 pt-6 sm:pt-6 font-mono'>
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-        <div className='mx-auto max-w-2xl text-center'>
-          <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+        <div className='mx-auto max-w-2xl text-center mb-2'>
+          <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl'>
             From the blog
           </h2>
           <p className='mt-2 text-lg leading-8 text-gray-600'>
             Latest updates on projects and builds
           </p>
         </div>
-        <div className='mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
+        <div className='mx-auto grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
           {posts.map((post) => (
             <PostCard post={post} key={post.meta.slug} />
           ))}
