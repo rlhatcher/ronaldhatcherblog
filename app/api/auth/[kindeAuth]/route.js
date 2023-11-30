446451b692de05c291b8f8e1ed9f21d3d96638f6
rlhatcher <ronaldhatcher@mac.com>
2023-11-30 13:30:22 +0000

    redo

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,6 +1,3 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
 
-export async function GET(request, {params}) {
-	const endpoint = params.kindeAuth;
-	return handleAuth(request, endpoint);
-}
+export const GET = handleAuth()
