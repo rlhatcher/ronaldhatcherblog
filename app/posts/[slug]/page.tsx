3499a725107e0d1c25a40a536cad25634d41c985
rlhatcher <ronaldhatcher@mac.com>
2023-12-03 23:35:21 +0000

    good start

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -51,8 +51,8 @@ export default async function PostPage ({
 
   const { meta, content } = post
   const tags = meta.tags.map((tag, i) => (
-    <div key={i} className='mb-2'>
-      <Link href={`/tags/${tag}`} className='p-1'>
+    <div key={i}>
+      <Link href={`/tags/${tag}`}>
         <Tag label={tag} />
       </Link>
     </div>
@@ -64,18 +64,30 @@ export default async function PostPage ({
         links={[{ href: '/posts', label: 'Posts' }]}
         page={{ title: meta.title }}
       />
-      <div className='bg-gray-100 rounded-2xl static'>
-        <div>
-          <CloudImage
-            title={meta.title}
-            image={meta.image}
-            className='rounded-xl mx-auto w-full'
-          />
+      <article className='relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900'>
+        <CloudImage
+          title={meta.title}
+          image={meta.image}
+          className='rounded-xl mx-auto w-full'
+        />
+        <div className='absolute bottom-5 left-0 right-0 w-full bg-gradient-to-r from-gray-900 to-transparent'>
+          <h3 className='text-lg font-semibold leading-6 text-white'>
+            {post.meta.description}
+          </h3>
+          <div className='container flex-wrap flex text-white flex-row font-mono justify-items-start gap-1 items-center px-5 py-2'>
+            <h4>Project</h4>
+            <Link href={`/projects/${post.meta.project}`}>
+              <span className='inline-flex items-center rounded-md bg-gray-50 px-1.5 py-0.5 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-900/100'>
+                {post.meta.project}
+              </span>
+            </Link>
+            <h4>Tags</h4>
+            {tags}
+          </div>
         </div>
-        <div className='flex flex-col'>{tags}</div>
-        <div className='prose prose-slate mx-auto max-w-full w-4/5 bg-white relative top-0 -mt-32 p-5 sm:p-10'>
-          {content}
-        </div>
+      </article>
+      <div className='prose prose-slate mx-auto max-w-full bg-white relative top-0  p-5 m:p-10'>
+        {content}
       </div>
     </div>
   )
