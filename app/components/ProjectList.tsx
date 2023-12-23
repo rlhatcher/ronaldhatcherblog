e7326a031d9650856f9e4fada324134e2c5a5a0a
rlhatcher <ronaldhatcher@mac.com>
2023-12-23 13:46:59 +0000

    looking better

--- app/components/ProjectList.tsx
+++ app/components/ProjectList.tsx
@@ -11,22 +11,25 @@ export default function ProjectList ({
   projects: Project[]
 }): React.JSX.Element {
   return (
-    <ul role='list' className='divide-y  divide-black'>
+    <ul role='list' className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
       {projects.map((project) => {
         return (
           <Link href={`/projects/${project.meta.slug}`} key={project.meta.slug}>
             <li
               key={project.meta.slug}
-              className='flex justify-between gap-x-6 py-5'
+              className='flex justify-between bg-white gap-x-6 py-5 px-5 mb-2  border-l-8 border-slate-500 rounded-sm'
             >
               <div className='flex min-w-0 gap-x-4 my-1'>
+              <div className='relative h-24 w-24 flex-none bg-gray-50'>
                 <CloudImage
-                  className='h-12 w-24 flex-none bg-gray-50'
+                  className='absolute top-0 left-0 h-full w-full object-cover rounded-md'
                   image={project.meta.image}
-                  height={48}
+                  height={96}
                   width={96}
+                  crop='thumb'
                   alt=''
                 />
+                </div>
                 <div className='min-w-0 flex-auto'>
                   <p className='text-lg font-semibold leading-6 text-gray-900'>
                     {project.meta.title}
@@ -36,7 +39,7 @@ export default function ProjectList ({
                   </p>
                 </div>
               </div>
-              <div className='hidden shrink-0 sm:flex sm:flex-col sm:items-end'>
+              <div className='hidden shrink-0  sm:flex sm:flex-col sm:items-end'>
                 <p className='text-sm leading-6 text-gray-900'>
                   {project.meta.tags.map((tag, i) => (
                     <div key={i}>
