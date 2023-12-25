b216dd4e926d62d3fb762a5b4fbf0d0e18007d4f
rlhatcher <ronaldhatcher@mac.com>
2023-12-25 23:16:26 +0000

    ftf

--- app/components/MfgCards.tsx
+++ app/components/MfgCards.tsx
@@ -14,7 +14,7 @@ export default async function MfgcCards ({
     <div className='mx-auto grid max-w-2xl auto-rows-fr grid-cols-1 gap-8  lg:max-w-none lg:grid-cols-3'>
       {manufacturers.map((manufacturer) => {
         return (
-          <Link href={`/data/manufacturers/${manufacturer.mfgID}`} key={manufacturer.mfgID} className='bg-gray-300 shadow-sm font-mono rounded-md p-2'>
+          <Link href={`/data/manufacturers/${manufacturer.mfgID}`} key={manufacturer.mfgID} className='bg-slate-200 border-l-8 border-slate-400 shadow-sm font-mono rounded-md p-2'>
             {manufacturer.name}
           </Link>
         )
