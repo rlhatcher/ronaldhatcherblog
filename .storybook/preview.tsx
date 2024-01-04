ffedb8019a32aabff817e9d641fa4e808105edd5
rlhatcher <ronaldhatcher@mac.com>
2024-01-04 16:34:30 +0000

    storybook working with one example

---
+++ .storybook/preview.tsx
@@ -0,0 +1,25 @@
+import type { Preview } from "@storybook/react";
+import "../app/globals.css";
+import { inter, courier } from "../lib/fonts";
+import React from "react";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className={`${inter.variable} ${courier.variable}`}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default preview;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import React from 'react'
-import { Inter, Courier_Prime } from 'next/font/google'
+import { inter, courier } from '@/lib/fonts'
 import GoogleAnalytics from './components/GoogleAnalytics'
 import { type Metadata } from 'next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
@@ -10,26 +10,13 @@ export const metadata: Metadata = {
   description: 'This is a blog.'
 }
 
-const mono = Courier_Prime({
-  weight: '400',
-  variable: '--font-courier',
-  subsets: ['latin'],
-  display: 'swap'
-})
-
-const sans = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap'
-})
-
 export default function RootLayout ({
   children
 }: {
   children: React.ReactNode
 }): JSX.Element {
   return (
-    <html lang='en' className={`${mono.variable} ${sans.variable}`}>
+    <html lang='en' className={`${courier.variable} ${inter.variable}`}>
       <body>
         <main>
           <GoogleAnalytics />
