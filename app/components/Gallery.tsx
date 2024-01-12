e941642521d2be16d6dcdad3f2302a39b9d337f9
rlhatcher <ronaldhatcher@mac.com>
2024-01-12 00:40:17 +0000

    simplify gallery markup

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -19,11 +19,9 @@ export default async function Gallery ({
 
   return (
     <div className='mx-auto max-w-full px-4 md:px-8'>
-      <div className='grid gap-4 md:gap-6 xl:gap-8'>
         {metas.map((meta) => (
           <GalleryItem imageMeta={meta} key={meta.assetId} />
         ))}
-      </div>
     </div>
   )
 }
--- app/components/GalleryItem.tsx
+++ app/components/GalleryItem.tsx
@@ -9,17 +9,8 @@ export default function GalleryItem ({
   imageMeta: ImageMeta
 }): React.JSX.Element {
   return (
-    <Link
-      href={`/images/${imageMeta.publicId}`}
-      className='relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80'
-    >
-      <CloudImage
-        image={imageMeta.publicId}
-        alt='Description of my image'
-        className='absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
-        height={600}
-      />
-      <div className='pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50'></div>
+    <Link href={`/images/${imageMeta.publicId}`}>
+      <CloudImage image={imageMeta.publicId} alt='Description of my image' />
     </Link>
   )
 }
