8d66578213205c936deae6b374fab2a526822418
rlhatcher <ronaldhatcher@mac.com>
2024-01-20 16:06:44 +0000

    profile tuning

--- app/ui/Profile.tsx
+++ app/ui/Profile.tsx
@@ -3,38 +3,17 @@
 The profile retrieves identity information from the IDP and retrieves the person information from the graph.
 */
 import React from 'react'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { mrgPerson } from '@/app/lib/neo4j'
+
 import ProfileBar from './dashboard/profile-bar'
+import getUser from '../lib/kinde'
 
 export default async function ProfilePage (): Promise<React.JSX.Element> {
-  const { getUser } = getKindeServerSession()
+  const user: Person = await getUser()
 
-  const user = await getUser()
-  if (
-    user?.id != null &&
-    user.email != null &&
-    user.given_name != null &&
-    user.family_name != null &&
-    user.picture != null
-  ) {
-    const person = await mrgPerson({
-      id: user?.id,
-      email: user?.email,
-      given_name: user?.given_name,
-      family_name: user?.family_name,
-      picture: user?.picture
-    })
-
-    const fullName = person?.given_name + ' ' + person?.family_name
-
-    return (
-      <div className='bg-gray-100 p-2'>
-        <ProfileBar name={fullName} />
-        <section></section>
-      </div>
-    )
-  } else {
-    return <div></div>
-  }
+  return (
+    <div className='bg-slate-200 p-2'>
+      <ProfileBar name={user.given_name + ' ' + user.family_name} />
+      <section></section>
+    </div>
+  )
 }
