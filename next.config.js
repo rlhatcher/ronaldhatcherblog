3da6a5a6293d2ba26efd00390979cbedaabcb31f
rlhatcher <ronaldhatcher@mac.com>
2024-03-04 22:25:32 +0000

    figuring out auth

--- next.config.js
+++ next.config.js
@@ -8,6 +8,13 @@ module.exports = {
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5254/api/:path*'
             : '/api/'
+      },
+      {
+        source: '/app/api/auth:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5254/api/:path*'
+            : '/app/api/auth/'
       }
     ]
   },
