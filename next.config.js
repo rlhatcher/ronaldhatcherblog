818fba80647794c0961ef863ab9732f91717bec0
rlhatcher <ronaldhatcher@mac.com>
2024-03-14 22:34:55 +0000

    api config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5254/api/rest/:path*'
-            : '/api/rest/'
+            : '/api/rest/:path*'
       }
     ]
   },
