ff98cef6899d95d0bf052638683d7f0b31e75f9d
rlhatcher <ronaldhatcher@mac.com>
2024-03-15 14:41:27 +0000

    remove api

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/rest/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5254/api/rest/:path*'
-            : '/api/rest/'
-      }
-    ]
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb'
