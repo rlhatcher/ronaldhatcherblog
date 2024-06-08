1583c9253dc12d4eee639eb4e0b3729aaccd8f0b
rlhatcher <ronaldhatcher@mac.com>
2024-06-08 20:35:40 +0100

    centre images

--- app/images/[...id]/page.tsx
+++ app/images/[...id]/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 import { BlogImage } from '@/components/cloud-image'
+import { getMetaByPublicId } from '@/lib/cloudinary'
 
-export const revalidate = 10
+// export const revalidate = 10
 
 interface Props {
   params: {
@@ -14,15 +15,19 @@ export default async function Image({
   params: { id },
 }: Props): Promise<React.JSX.Element> {
   const publicId = id.join('/')
+  const meta = await getMetaByPublicId(publicId)
 
   return (
-    <div className="container mx-auto sm:px-8 lg:px-10">
-      <BlogImage
-        src={publicId}
-        width="800"
-        height="600"
-        alt="TBD"
-      />
-    </div>
+    <>
+      <div className="container mx-auto sm:px-8 lg:px-10">
+        <BlogImage
+          src={publicId}
+          width="800"
+          height="600"
+          alt="TBD"
+        />
+      </div>
+      <div>{meta.assetId}</div>
+    </>
   )
 }
