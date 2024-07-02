c35ed14fb068ccc62a962a747b77a1a82f128b3f
rlhatcher <ronaldhatcher@mac.com>
2024-07-03 00:22:10 +0100

    fix step styling

--- app/builds/[slug]/[step]/page.tsx
+++ app/builds/[slug]/[step]/page.tsx
@@ -1,8 +1,7 @@
 import { notFound } from 'next/navigation'
 import React from 'react'
 
-import { BreadcrumbResponsive } from '@/components/bread-crumb'
-import { CloudImage } from '@/components/cloud-image'
+import MdxPage from '@/components/mdx-page'
 import { getBuildsMeta } from '@/lib/github/builds'
 import { getStepsMeta, getStepByName } from '@/lib/github/steps'
 
@@ -54,27 +53,10 @@ export default async function StepPage({
   ]
 
   return (
-    <div className="container mx-auto sm:px-6 lg:px-8">
-      <BreadcrumbResponsive items={links} />
-      <article className="prose relative isolate flex flex-col justify-end overflow-hidden border dark:prose-invert prose-h1:mb-0 prose-h1:font-mono prose-ul:m-0 prose-li:m-0">
-        <CloudImage
-          title="build"
-          src={meta.image}
-          className="mx-auto w-full"
-          crop="thumb"
-          alt=""
-          width={meta.imageWidth}
-          height={meta.imageHeight}
-        />
-        <div className="absolute bottom-5 left-0 right-0 w-full bg-gradient-to-r from-gray-900 to-transparent">
-          <h3 className="px-2 text-lg font-semibold leading-6 text-white">
-            {meta.description}
-          </h3>
-        </div>
-      </article>
-      <div className="m:p-10 prose prose-slate relative top-0 mx-auto max-w-full bg-slate-50 p-5">
-        {content}
-      </div>
-    </div>
+    <MdxPage
+      meta={meta}
+      content={content}
+      links={links}
+    />
   )
 }
