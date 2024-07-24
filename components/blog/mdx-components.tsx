142dffeaffbb373492c1fb0eb1de708a38fd1b41
rlhatcher <ronaldhatcher@mac.com>
2024-07-24 21:21:14 +0100

    dsd

---
+++ components/blog/mdx-components.tsx
@@ -0,0 +1,16 @@
+import { Rocket3DViewer } from '../model-viewers'
+
+import SimTabs from '@/components/blog/simulations'
+import { VideoPlayer, BlogImage, BlogGallery } from '@/components/cloud-image'
+import Video from '@/components/video'
+
+const MdxComponents = {
+  VideoPlayer,
+  BlogImage,
+  BlogGallery,
+  SimTabs,
+  Video,
+  Rocket3DViewer,
+}
+
+export default MdxComponents
